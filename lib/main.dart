9386d2c6a299346c08ce9508efd70672bb709e43
Iqbal <iqbalmineraltown@gmail.com>
2021-12-16 19:15:18 +0700

    main layout

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:iqbalmineraltown_github_io/src/home.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyPortfolioApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyPortfolioApp extends StatelessWidget {
+  const MyPortfolioApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,19 +16,29 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.dark,
       darkTheme: Theme.of(context).copyWith(
-        platform: TargetPlatform.android,
-        scaffoldBackgroundColor: _bgColor,
-        primaryColor: _primaryColor,
-        canvasColor: _bgColor,
-        textTheme: GoogleFonts.averiaLibreTextTheme(),
+        textTheme: GoogleFonts.montserratTextTheme(),
       ),
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Home(),
+      home: const _PortfolioPage(),
     );
   }
 }
 
-const Color _primaryColor = Color(0xFF1122AA);
-const Color _bgColor = Color(0xFF010122);
+class _PortfolioPage extends StatelessWidget {
+  const _PortfolioPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Home(), // short introduction, photo?
+        Home(), // longer text, about & background
+        Home(), // skills & specialties
+        Home(), // portfolios
+        Home(), // email contacts, network links
+      ],
+    );
+  }
+}
--- lib/src/home.dart
+++ lib/src/home.dart
@@ -1,46 +1,23 @@
 import 'package:flutter/material.dart';
 
-import 'package:google_fonts/google_fonts.dart';
-
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            _Header(),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class _Header extends StatelessWidget {
-  const _Header({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return MouseRegion(
-      cursor: SystemMouseCursors.click,
-      child: Row(
-        children: [
-          Container(
-            color: Colors.green,
-            child: Text(
-              'Hai, saya John Doe',
-              style: GoogleFonts.roboto(
-                color: Colors.white,
-                fontWeight: FontWeight.w800,
-                height: 1.8,
-                fontSize: 18.0,
-              ),
-            ),
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    return Container(
+      height: height,
+      width: width,
+      child: Container(
+        color: Colors.amber,
+        child: Center(
+          child: Text(
+            'John Thor',
+            style: Theme.of(context).textTheme.headline1,
           ),
-        ],
+        ),
       ),
     );
   }
